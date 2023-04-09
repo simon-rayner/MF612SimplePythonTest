0b29ab42e8bbbb3fd495e82a83fa075434e61c2d
SimonR <simon.rayner@medisin.uio.no>
2023-04-09 15:01:47 +0200

    first commit

---
+++ addTwoNumbers.py
@@ -0,0 +1,9 @@
+# Store input numbers
+n1 = input('Enter number 1: ')
+n2 = input('Enter number 2: ')
+
+# Add two numbers
+sum = float(n1) + float(n2)
+
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(n1, n2, sum))
